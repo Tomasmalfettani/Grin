768c09046a18740d31ed0d18c4cf00ada9d4dca2
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-06-16 00:24:41 -0300

    Agregue scripts de scroll y 3 textos

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.animated', {
+      delay: 200,
+      duration: 800,
+      origin: 'bottom',
+      distance: '20px',
+      easing: 'ease-out',
+      reset: true
+    });
+  });
+  
